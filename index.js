c34832327cf1f03b2992eced7920d07ba2e3332c
Akshay Shetty <shettyakshay744@gmail.com>
2023-02-14 21:55:37 +0530

    ToDo App

--- index.js
+++ index.js
@@ -1,98 +1,118 @@
 let count = document.querySelector(".count");
+let arr = [];
+let obj = {};
+
 function creating() {
   // Div item
   let item = document.createElement("div");
-  item.classList.add("itemdiv");
+       item.classList.add("itemdiv");
 
   // checkbox
-  let checkBtn = document.createElement("INPUT");
-  checkBtn.setAttribute("type", "checkbox");
-  checkBtn.setAttribute("value", "0");
-  checkBtn.setAttribute("name", "fordCheckBox");
-  checkBtn.classList.add("checkBtn");
+  let  checkBtn = document.createElement("INPUT");
+        checkBtn.setAttribute("type", "checkbox");
+        checkBtn.setAttribute("value", "0");
+        checkBtn.setAttribute("name", "fordCheckBox");
+        checkBtn.classList.add("checkBtn");
 
   // creating p tag for  input value store in p tag
   let textinput = document.createElement("p");
-  textinput.classList.add("textinput");
+      textinput.classList.add("textinput");
 
   //  Delete button
-  let DeleteButton = document.createElement("button");
-  DeleteButton.textContent = "x";
-  DeleteButton.classList.add("DeleteButton");
+  let  deleteButton = document.createElement("button");
+        deleteButton.textContent = "x";
+        deleteButton.classList.add("deleteButton");
 
   let top = document.querySelector(".top");
 
-  if (event.key === "Enter") {
-    textinput.innerText = input.value;
-    item.appendChild(textinput);
-    item.appendChild(checkBtn);
-    item.appendChild(DeleteButton);
-    if (input.value) {
-      top.appendChild(item);
-    }
-    input.value = "";
+  if (event.key === "Enter" && input.value) {
+        textinput.innerText = input.value;
+        item.appendChild(textinput);
+        item.appendChild(checkBtn);
+        item.appendChild(deleteButton);
+
+    obj = {
+        value: input.value,
+        status: false,
+    };
+        arr.push(obj);
+        top.appendChild(item);
+
+        input.value = "";
   }
 }
 
 var input = document.querySelector(".inputBox");
-input.addEventListener("keypress", function () {
-  creating();
+      input.addEventListener("keypress", function () {
+      creating();
+
   let checkBtn = document.querySelectorAll(".checkBtn");
-  count.textContent = checkBtn.length + " item left";
+       count.textContent = checkBtn.length + " item left";
 });
 
 // Delete button
 let deleteBtn = document.querySelector(".top");
-deleteBtn.addEventListener("click", (e) => {
-  if (e.target.className == "DeleteButton") {
+     deleteBtn.addEventListener("click", (e) => {
+  if (e.target.className == "deleteButton") {
     let parent = e.target.parentElement;
-    parent.remove();
+         parent.remove();
   }
 });
 
 // select items
 
 let check = document.querySelector(".top");
-check.addEventListener("change", (e) => {
+check.addEventListener("change", (e, i) => {
   // Checkbox is checked..
   if (e.target.checked) {
+    let val = e.target.parentElement.children[0].innerText;
+    let data = arr.forEach((ele) => {
+      if (val == ele.value) {
+        console.log(ele.value);
+        obj.status = true;
+      }
+    });
+
+
     // clear completed
     let checked = document.querySelectorAll(".checked");
     if (checked.length >= 0) {
       let show = document.querySelector(".Clear-Completed");
-      show.style.visibility = "visible";
-
+           show.style.visibility = "visible";
       // complete clear
       let clear = document.querySelector(".Clear-Completed");
-      clear.addEventListener("click", () => {
+           clear.addEventListener("click", () => {
         e.target.parentElement.remove();
-        console.log();
+        let checked = document.querySelectorAll(".checkBtn");
+        checked.forEach((e) => {
+          e.parentElement.style.display = "";
+        });
       });
     }
 
     if (e.target.className == "checkBtn") {
-      let parent = e.target.parentElement;
-      parent.childNodes[0].style.textDecoration = "line-through";
-      parent.childNodes[0].style.color = "gray";
-      parent.childNodes[1].classList = "checked";
+      let  parent = e.target.parentElement;
+            parent.childNodes[0].style.textDecoration = "line-through";
+            parent.childNodes[0].style.color = "gray";
+            parent.childNodes[1].classList = "checked";
       let checkBtn = document.querySelectorAll(".checkBtn");
-      count.textContent = checkBtn.length + " item left";
+           count.textContent = checkBtn.length + " item left";
     }
   }
-    
+
   // Checkbox is Unchecked..
   else {
     let parent = e.target.parentElement;
-    parent.childNodes[0].style.textDecoration = "none";
-    parent.childNodes[0].style.color = "black";
-    parent.childNodes[1].classList = "checkBtn";
- let checkBtn1 = document.querySelectorAll(".checkBtn");
- count.textContent = checkBtn1.length + " item left";
+        parent.childNodes[0].style.textDecoration = "none";
+        parent.childNodes[0].style.color = "black";
+        parent.childNodes[1].classList = "checkBtn";
+    let checkBtn1 = document.querySelectorAll(".checkBtn");
+         count.textContent = checkBtn1.length + " item left";
     // clear completed
     let checked = document.querySelectorAll(".checked");
     if (checked.length <= 0) {
       let show = document.querySelector(".Clear-Completed");
-      show.style.visibility = "hidden";
+           show.style.visibility = "hidden";
     }
   }
 });
@@ -100,7 +120,10 @@ check.addEventListener("change", (e) => {
 // All
 let All = document.querySelector(".mainDiv");
 All.addEventListener("click", (e) => {
-  if (e.target.className == "All") {
+    if (e.target.className == "All") {
+        e.target.style.cssText = "color:red;"
+        document.querySelector(".Completed").style.cssText = "color:black ; "
+        document.querySelector(".Active").style.cssText="color:black ; "
     let checkBtn = document.querySelectorAll(".checkBtn");
     checkBtn.forEach((e) => {
       e.parentElement.style.display = "";
@@ -110,14 +133,18 @@ All.addEventListener("click", (e) => {
 
 // Active
 let Active = document.querySelector(".mainDiv");
-Active.addEventListener("click", (e) => {
+     Active.addEventListener("click", (e) => {
   let checked = document.querySelectorAll(".checked");
-  if (e.target.className == "Active") {
-    checked.forEach((e) => {
+    if (e.target.className == "Active") {
+        document.querySelector(".All").style.cssText = "color:black ; "
+        document.querySelector(".Completed").style.cssText="color:black ; "
+        document.querySelector(".All").style.cssText="color:black ; "
+        e.target.style.cssText = "color:red;"
+     checked.forEach((e) => {
       e.parentElement.style.display = "none";
     });
   } else {
-    checked.forEach((e) => {
+     checked.forEach((e) => {
       e.parentElement.style.display = "";
     });
   }
@@ -125,29 +152,46 @@ Active.addEventListener("click", (e) => {
 
 // Completed
 let Completed = document.querySelector(".mainDiv");
-Completed.addEventListener("click", (e) => {
-  if (e.target.className == "Completed") {
+     Completed.addEventListener("click", (e) => {
+    if (e.target.className == "Completed") {
+        e.target.style.cssText = "color:red;"
+        document.querySelector(".All").style.cssText = "color:black ; "
+        document.querySelector(".Active").style.cssText="color:black ; "
     let checkBtn = document.querySelectorAll(".checkBtn");
-    checkBtn.forEach((e) => {
+         checkBtn.forEach((e) => {
       e.parentElement.style.display = "none";
     });
   } else {
     let checked = document.querySelectorAll(".checkBtn");
-    checked.forEach((e) => {
-      e.parentElement.style.display = "";
+         checked.forEach((e) => {
+         e.parentElement.style.display = "";
     });
   }
 });
 
-let selectAll = document.querySelector(".mainDiv");
-selectAll.addEventListener("click", (e) => {
-    
-    if (e.target.className == "selectAll") {
-      let chileClass = document.querySelectorAll(".itemdiv");
-      chileClass.forEach((ele) => {
-          ele.childNodes[1].setAttribute("class", "checked");
-          console.log(ele.childNodes[1]);
-       })
-    }
+// let selectAll = document.querySelector(".mainDiv");
+// selectAll.addEventListener("click", (e) => {
+//   if (e.target.className == "selectAll") {
+//     if (e.target.checked) {
+//       let chileClass = document.querySelectorAll(".itemdiv");
+//       chileClass.forEach((ele) => {
+//         ele.childNodes[1].setAttribute("checked", "checked");
+//       });
+//     } else {
+//       let chileClass = document.querySelectorAll(".itemdiv");
+//       chileClass.forEach((ele) => {
+//         ele.childNodes[1].removeAttr("checked");
+//         console.log("false");
+//       });
+//     }
+//   }
+// });
 
-})
+// arr.push(obj);
+// let k = document.querySelectorAll(".top");
+// k.forEach((e,i) => {
+//     console.log(e.children[i].children[0]);
+//     // return arr == e
+// })
+arr.shift();
+console.log(arr);
